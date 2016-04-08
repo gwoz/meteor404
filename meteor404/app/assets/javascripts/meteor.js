63f1baefb27341427ad0266b61a27cca931bb659
Akeem Street & Mike Gwozdek & Tim Beck <pair+akeem-s.gwoz.timbek2@devbootcamp.com>
2016-04-08 10:53:10 -0400

    Add css and google map markers. Convert aspects of google map code to oojs

--- meteor404/app/assets/javascripts/meteor.js
+++ meteor404/app/assets/javascripts/meteor.js
@@ -1,14 +1,31 @@
-$(document).ready(function(){
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7128, lng: -74.0059},
+    zoom: 6
+  });
 
+var Postion = function(usrLat, usrLng){
+   new google.maps.Marker({
+    position:{lat: usrLat, lng: usrLng},
+    map: map,
+    title: 'meteor'
+  });
+}
+  new Postion(40.46579843567, -75);
+  new Postion(40.5, -75.23);
+  new Postion(40.8, -75.49);
+  new Postion(40.9, -75.1241);
+  new Postion(40.1, -75.6);
+}
+
+$(document).ready(function(){
   $.ajax({
-    url: 'https://data.nasa.gov/resource/gh4g-9sfh.json',
-    data: {"id": "392"}
+    url: 'https://data.nasa.gov/resource/gh4g-9sfh.json'
   }).done(function(response){
     var test = $(response)
-    console.log(test)
+    for (var i = 0; i < response.length; i ++){
+      new Position(response[i].reclat, response[i].reclong)
+    }
   })
-
-
-
-
 })
--- meteor404/app/models/address.rb
+++ meteor404/app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :user
+
+  def concat_address
+    self.street + self.city + self.state + self.country
+  end
+
 end
