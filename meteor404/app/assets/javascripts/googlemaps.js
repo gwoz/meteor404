a73990b69dd767c47d29b09606971df9ed827d9a
Mike Gwozdek <mikegwozdek@gmail.com>
2016-04-08 12:10:23 -0400

    Revert "Create meteors"

--- meteor404/app/assets/javascripts/googlemaps.js
+++ meteor404/app/assets/javascripts/googlemaps.js
@@ -1,21 +1,7 @@
-// var map;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: 40.7128, lng: -74.0059},
-//     zoom: 6
-//   });
-
-// var Postion = function(usrLat, usrLng){
-//    new google.maps.Marker({
-//     position:{lat: usrLat, lng: usrLng},
-//     map: map,
-//     title: 'meteor'
-//   });
-// }
-//   new Postion(40.46579843567, -75);
-//   new Postion(40.5, -75.23);
-//   new Postion(40.8, -75.49);
-//   new Postion(40.9, -75.1241);
-//   new Postion(40.1, -75.6);
-// }
-
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
--- meteor404/app/models/address.rb
+++ meteor404/app/models/address.rb
@@ -1,8 +1,3 @@
 class Address < ActiveRecord::Base
   belongs_to :user
-
-  def concat_address
-    self.street + self.city + self.state + self.country
-  end
-
 end
