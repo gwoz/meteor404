aee40ba6677af8a87b1e9df59328a7af8ba1975a
Akeem Street & Mike Gwozdek & Tim Beck <pair+akeem-s.gwoz.timbek2@devbootcamp.com>
2016-04-08 12:09:32 -0400

    Add ability to display meteor data on map

--- meteor404/app/assets/javascripts/meteor.js
+++ meteor404/app/assets/javascripts/meteor.js
@@ -1,26 +1,5 @@
 meteorArray = []
 
-var Meteor = function(lat, long){
-  this.lat = lat;
-  this.long = long;
-}
-
-$(document).ready(function(){
-  $.ajax({
-    url: 'https://data.nasa.gov/resource/gh4g-9sfh.json'
-  }).done(function(response){
-
-
-  for (var i = 0; i < response.length; i ++){
-
-    var meteor = new Meteor(response[i].reclat, response[i].reclong)
-    meteorArray.push(meteor)
-    } //closes for loop
-    debugger;
-    console.log(meteorArray)
-  }) //closes done
-}) //closes document.ready
-
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -28,17 +7,34 @@ function initMap() {
     zoom: 6
   });
 
-var Postion = function(usrLat, usrLng){
-   new google.maps.Marker({
-    position:{lat: usrLat, lng: usrLng},
-    map: map,
-    title: 'meteor'
-  });
-}
-  console.log(meteorArray[0])
-  // new Postion(40.46579843567, -75);
-  // new Postion(40.5, -75.23);
-  // new Postion(40.8, -75.49);
-  // new Postion(40.9, -75.1241);
-  // new Postion(40.1, -75.6);
+  var Meteor = function(lat, lng){
+    this.lat = lat;
+    this.lng = lng;
+  }
+
+  var Position = function(usrLat, usrLng){
+     new google.maps.Marker({
+      position:{lat: usrLat, lng: usrLng},
+      map: map,
+      title: 'meteor'
+    });
+  }
+
+  var populateMeteors = function(response){
+    for(var i = 0; i < response.length; i ++){
+      meteorArray.push(new Meteor(parseInt(response[i].reclat), parseInt(response[i].reclong)))
+    }
+    return meteorArray;
+  }
+
+  $(document).ready(function(){
+    $.ajax({
+      url: 'https://data.nasa.gov/resource/gh4g-9sfh.json'
+    }).done(function(response){
+      populateMeteors(response);
+      for (var i = 0; i < meteorArray.length; i ++){
+        new Position(meteorArray[i].lat, meteorArray[i].lng)
+      }
+    })
+  })
 }
