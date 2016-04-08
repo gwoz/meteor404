a1f5a8202bb8d029ba22671ed46b87ad0c3d7c6c
Akeem Street & Mike Gwozdek & Tim Beck <pair+akeem-s.gwoz.timbek2@devbootcamp.com>
2016-04-08 11:16:39 -0400

    Convert response to meteor object in an array.

--- meteor404/app/assets/javascripts/meteor.js
+++ meteor404/app/assets/javascripts/meteor.js
@@ -1,3 +1,26 @@
+meteorArray = []
+
+var Meteor = function(lat, long){
+  this.lat = lat;
+  this.long = long;
+}
+
+$(document).ready(function(){
+  $.ajax({
+    url: 'https://data.nasa.gov/resource/gh4g-9sfh.json'
+  }).done(function(response){
+
+
+  for (var i = 0; i < response.length; i ++){
+
+    var meteor = new Meteor(response[i].reclat, response[i].reclong)
+    meteorArray.push(meteor)
+    } //closes for loop
+    debugger;
+    console.log(meteorArray)
+  }) //closes done
+}) //closes document.ready
+
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -12,20 +35,10 @@ var Postion = function(usrLat, usrLng){
     title: 'meteor'
   });
 }
-  new Postion(40.46579843567, -75);
-  new Postion(40.5, -75.23);
-  new Postion(40.8, -75.49);
-  new Postion(40.9, -75.1241);
-  new Postion(40.1, -75.6);
+  console.log(meteorArray[0])
+  // new Postion(40.46579843567, -75);
+  // new Postion(40.5, -75.23);
+  // new Postion(40.8, -75.49);
+  // new Postion(40.9, -75.1241);
+  // new Postion(40.1, -75.6);
 }
-
-$(document).ready(function(){
-  $.ajax({
-    url: 'https://data.nasa.gov/resource/gh4g-9sfh.json'
-  }).done(function(response){
-    var test = $(response)
-    for (var i = 0; i < response.length; i ++){
-      new Position(response[i].reclat, response[i].reclong)
-    }
-  })
-})
