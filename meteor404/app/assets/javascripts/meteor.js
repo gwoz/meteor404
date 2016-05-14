3172069b1222449aa384b7be2f7a52c4a6c0f959
akeem-s <akeemstreet@gmail.com>
2016-05-14 16:21:22 -0400

    Made marker clickable. Need to change format of for loop to populate map and make all clickable.

--- meteor404/app/assets/javascripts/meteor.js
+++ meteor404/app/assets/javascripts/meteor.js
@@ -7,24 +7,25 @@ function initMap() {
     zoom: 6
   });
 
-
-  var Meteor = function(lat, lng){
+  var Meteor = function(lat, lng, name){
     this.lat = lat;
     this.lng = lng;
+    this.name = name;
   }
 
-  var Position = function(usrLat, usrLng){
+  var Position = function(usrLat, usrLng, name){
      new google.maps.Marker({
       position:{lat: usrLat, lng: usrLng},
       map: map1,
       title: 'meteor',
+      customInfo: name
       // icon: "<img src='/images/rock.png'>"
     });
   }
 
   var populateMeteors = function(response){
     for(var i = 0; i < response.length; i ++){
-      meteorArray.push(new Meteor(parseFloat(response[i].reclat), parseFloat(response[i].reclong)))
+      meteorArray.push(new Meteor(parseFloat(response[i].reclat), parseFloat(response[i].reclong), response[i].name))
     }
     return meteorArray;
   }
@@ -35,19 +36,28 @@ function initMap() {
       data: {fall: "Fell"}
     }).done(function(response){
       populateMeteors(response);
-
-      for (var i = 0; i < meteorArray.length; i ++){
-        new Position(meteorArray[i].lat, meteorArray[i].lng)
-      }
-    })
+      // for loop creating all meteor objects
+      // for (var i = 0; i < meteorArray.length; i ++){
+      //   new Position(meteorArray[i].lat, meteorArray[i].lng, meteorArray[i].name)
+      // }
+      //var marker = new Position(meteorArray[0].lat, meteorArray[0].lng, meteorArray[0].name)
+      var marker = new google.maps.Marker({
+      position:{lat: meteorArray[0].lat, lng: meteorArray[0].lng},
+      map: map1,
+      title: 'meteor',
+      customInfo: meteorArray[0].name
+      // icon: "<img src='/images/rock.png'>"
+    });
+      marker.addListener('click', function() {
+      alert("jfwiojoi");
+      })
+    });
   })
 
-  // search bar
-  var input = document.getElementById('google-search-bar');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  
+  // // search bar
+  // var input = document.getElementById('google-search-bar');
+  // var searchBox = new google.maps.places.SearchBox(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 }
 
 
