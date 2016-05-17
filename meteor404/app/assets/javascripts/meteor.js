494e69c18b674c1048bc74482ce930b3e089aa48
TimBek2 <tim.beck46@gmail.com>
2016-05-17 13:15:36 -0400

    Remove redundant variable names from view file

--- meteor404/app/assets/javascripts/meteor.js
+++ meteor404/app/assets/javascripts/meteor.js
@@ -1,22 +1,23 @@
 meteorArray = []
 
-var map1;
+var map;
 function initMap() {
-  map1 = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.7128, lng: -74.0059},
     zoom: 6
   });
 
 
-  var Meteor = function(lat, lng){
+  var Meteor = function(lat, lng, name){
     this.lat = lat;
     this.lng = lng;
+    this.name = name;
   }
 
   var Position = function(usrLat, usrLng){
      new google.maps.Marker({
       position:{lat: usrLat, lng: usrLng},
-      map: map1,
+      map: map,
       title: 'meteor',
       // icon: "<img src='/images/rock.png'>"
     });
@@ -24,7 +25,7 @@ function initMap() {
 
   var populateMeteors = function(response){
     for(var i = 0; i < response.length; i ++){
-      meteorArray.push(new Meteor(parseFloat(response[i].reclat), parseFloat(response[i].reclong)))
+      meteorArray.push(new Meteor(parseFloat(response[i].reclat), parseFloat(response[i].reclong), response[i].name))
     }
     return meteorArray;
   }
@@ -35,7 +36,6 @@ function initMap() {
       data: {fall: "Fell"}
     }).done(function(response){
       populateMeteors(response);
-
       for (var i = 0; i < meteorArray.length; i ++){
         new Position(meteorArray[i].lat, meteorArray[i].lng)
       }
@@ -46,6 +46,11 @@ function initMap() {
   var input = document.getElementById('google-search-bar');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // bias search bar to current window
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
 
   
 }
