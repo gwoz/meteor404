a889e34212ae58e4ac55298c10bef6303e842f75
akeem-s <akeemstreet@gmail.com>
2016-05-29 07:34:37 -0400

    Remove unneccesary code from addresses create route

--- meteor404/app/controllers/addresses_controller.rb
+++ meteor404/app/controllers/addresses_controller.rb
@@ -22,21 +22,7 @@ class AddressesController < ApplicationController
       @step_list << step["html_instructions"]
     end
 
-    # @step_list_string = JSON.stringify(@step_list)
-    # JSON.parse(@step_list_string)
-    # respond_to do |format|
-    #   format.json { render :json => @step_list.to_json }
-    #   # format.json { step_list @step_list }
-    # end
-
     render json: @step_list 
-
-
-
-
-    # @address = Address.new(street: params[:address][:street], state: params[:address][:state], city: params[:address][:city], country: params[:address][:country], user_id: 1)
-    # @address.save
-
   end
 
   def show
